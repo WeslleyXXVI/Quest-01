e18af117cbc1bdea4c4c1734752f3575fbceb82e
Weslley Almeida <86155409+WeslleyXXVI@users.noreply.github.com>
2022-01-29 19:49:03 -0300

    Criação questão 02
    Criação da solução da questão 02

---
+++ question02.py
@@ -0,0 +1,18 @@
+numero = int(input("Digite um numero: "))
+primeiro = 0
+segundo = 1
+soma = 0
+i = 0
+lista = []
+lista.append(primeiro)
+lista.append(segundo)
+while soma < 10000:
+    soma = int(lista[i]) + int(lista[i+1])
+    primeiro = int(lista[i+1])
+    segundo = soma
+    lista.append(segundo)
+    i += 1
+if numero in lista:
+    print(f"O número {numero} pertece à sequencia de Fibonacci")
+else:
+    print(f"O número {numero} não pertece à sequencia de Fibonacci")
